2ed9592a56a63d4fbdda2e1e4761ca62eda6addd
salvadord <salvadordura@gmail.com>
2018-05-15 01:31:27 -0700

    added tuts, cells and mod

---
+++ gui_tut2.py
@@ -0,0 +1,59 @@
+from netpyne import specs
+
+# Network parameters
+netParams = specs.NetParams()  # object of class NetParams to store the network parameters
+
+## Population parameters
+netParams.popParams['E'] = {'cellType': 'PT', 'numCells': 3, 'cellModel':''}
+netParams.popParams['I'] = {'cellType': 'FS', 'numCells': 3, 'cellModel':''}
+
+
+## Cell property rules
+netParams.importCellParams(label='PT_rule', conds={'cellType': 'PT'}, fileName='/home/jovyan/netpyne_workspace/cells/PTcell.py', cellName='PTcell') 
+netParams.importCellParams(label='FS_rule', conds={'cellType': 'FS'}, fileName='/home/jovyan/netpyne_workspace/cells/FScell.py', cellName='FScell') 
+
+# Stimulation parameters
+netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 20, 'noise': 0.0, 'start': 1}
+netParams.stimTargetParams['bkg->PYR1'] = {'source': 'bkg', 'sec': 'soma', 'conds': {'pop': 'E'}, 'weight': 0.1, 'delay': 5}
+
+
+# Synaptic mechanism parameters
+netParams.synMechParams['AMPA'] = {'mod': 'Exp2Syn', 'tau1': 0.5, 'tau2': 1.0, 'e': 0}
+netParams.synMechParams['GABA'] = {'mod': 'Exp2Syn', 'tau1': 0.5, 'tau2': 1.0, 'e': -100}
+
+
+# Connectivity parameters
+netParams.connParams['E->I'] = {
+    'preConds': {'pop': 'E'}, 'postConds': {'pop': ['I']},
+    'weight': 0.03,                    # weight of each connection
+    'delay': 5,     
+    'synMech': 'AMPA',
+    'sec': 'soma'}
+
+netParams.connParams['I->all'] = {
+     'preConds': {'pop': 'I'}, 'postConds': {'pop': ['E']},
+     'weight': 0.09,                    # weight of each connection
+     'delay': 40,     
+     'synMech': 'GABA',
+     'sec': 'soma'}
+
+
+# Simulation options
+simConfig = specs.SimConfig()		# object of class SimConfig to store simulation configuration
+
+simConfig.duration = 0.2*1e3 			# Duration of the simulation, in ms
+simConfig.dt = 0.1				# Internal integration timestep to use
+simConfig.verbose = False  			# Show detailed messages 
+simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
+simConfig.recordStep = 1 			# Step size in ms to save data (eg. V traces, LFP, etc)
+simConfig.filename = 'model_output'  # Set file output name
+simConfig.savePickle = False 		# Save params, network and sim output to pickle file
+simConfig.recordCells = [0]
+
+simConfig.analysis['plotSpikeHist'] =  {'include': ['E','I'], 'yaxis': 'count'}
+simConfig.analysis['plotTraces']={'include': [0]}
+
+#from netpyne import sim
+#sim.createSimulateAnalyze()
+
+
