76858422df8ea9c76853a3296c5885878a1b82be
salvadord <salvadordura@gmail.com>
2018-07-03 16:57:59 -0400

    tested ip3->kBK + extracellular

--- gui_tut3_rxd.py
+++ gui_tut3_rxd.py
@@ -14,12 +14,12 @@ netParams.propVelocity = 100.0 # propagation velocity (um/ms)
 netParams.probLengthConst = 150.0 # length constant for conn probability (um)
 
 ## Population parameters
-netParams.popParams['E2'] = {'cellType': 'E', 'numCells': 1, 'yRange': [100,300], 'cellModel': 'HH'}
-# netParams.popParams['I2'] = {'cellType': 'I', 'numCells': 1, 'yRange': [100,300], 'cellModel': 'HH'}
-# netParams.popParams['E4'] = {'cellType': 'E', 'numCells': 1, 'yRange': [300,600], 'cellModel': 'HH'}
-# netParams.popParams['I4'] = {'cellType': 'I', 'numCells': 1, 'yRange': [300,600], 'cellModel': 'HH'}
-# netParams.popParams['E5'] = {'cellType': 'E', 'numCells': 1, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}
-# netParams.popParams['I5'] = {'cellType': 'I', 'numCells': 1, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}
+netParams.popParams['E2'] = {'cellType': 'E', 'numCells': 20, 'yRange': [100,300], 'cellModel': 'HH'}
+netParams.popParams['I2'] = {'cellType': 'I', 'numCells': 20, 'yRange': [100,300], 'cellModel': 'HH'}
+netParams.popParams['E4'] = {'cellType': 'E', 'numCells': 20, 'yRange': [300,600], 'cellModel': 'HH'}
+netParams.popParams['I4'] = {'cellType': 'I', 'numCells': 20, 'yRange': [300,600], 'cellModel': 'HH'}
+netParams.popParams['E5'] = {'cellType': 'E', 'numCells': 20, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}
+netParams.popParams['I5'] = {'cellType': 'I', 'numCells': 20, 'ynormRange': [0.6,1.0], 'cellModel': 'HH'}
 
 ## Cell property rules
 netParams.loadCellParamsRule(label='CellRule', fileName='cells/IT2_reduced_cellParams.json')
@@ -29,17 +29,23 @@ netParams.cellParams['CellRule']['conds'] = {'cellType': ['E','I']}
 '''
 kBK - hyperpolarizing, + ica, ca flows out = cai decreases
 cat - depolarizing, -ica, ca flows in = cai increases -- uses ghk eqn but can't get to be affected by concetration
+can,cal - same as cat but depends inversely on ica -- if high ica then low 
+
+high ip3 -> ER Ca released to Cyt -> kBK channels open -> less firing
+
+example of Ca reducing activity: https://onlinelibrary.wiley.com/doi/pdf/10.1002/wmts.34
+example of Ca increasing activity: https://www.sciencedirect.com/science/article/pii/S0006349598778324
 
 '''
-caInc = 10 # 1000
-caMechParams = {'cal': ['gcalbar', caInc], 'can': ['gcanbar', caInc], 'cat': ['gcatbar', caInc], 'kBK': ['gpeak', caInc]} #cal 0.0005 can 0.0001 cat 0.00075
+caInc = 1 # 1000
+caMechParams = {'cal': ['gcalbar', 1], 'can': ['gcanbar', 1], 'cat': ['gcatbar', 1], 'kBK': ['gpeak', 5], 'nax':['gbar', 1]} #cal 0.0005 can 0.0001 cat 0.00075
 secs = ['soma', 'Adend1', 'Adend2', 'Adend3', 'Bdend']
 for k,v in caMechParams.iteritems():
     for sec in secs:
         netParams.cellParams['CellRule']['secs']['soma']['mechs'][k][v[0]] *= v[1]
         
 # remove channels
-removeMechs = ['cadad', 'cat', 'cal', 'can']#, 'nax']
+removeMechs = ['cadad',  'cat']#, 'nax']#'cal', 'kBK' 'can'] # 'nax', kBK'] #
 secs = ['soma', 'Adend1', 'Adend2', 'Adend3', 'Bdend']
 for mech in removeMechs:
     for sec in secs:
@@ -55,7 +61,7 @@ netParams.synMechParams['inh'] = {'mod': 'Exp2Syn', 'tau1': 0.6, 'tau2': 8.5, 'e
 
 # Stimulation parameters
 netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 40, 'noise': 0.3}
-netParams.stimTargetParams['bkg->all'] = {'source': 'bkg', 'conds': {'cellType': ['E','I']}, 'weight': 10.0, 'sec': 'soma', 'delay': 'max(1, normal(5,2))', 'synMech': 'exc'}
+netParams.stimTargetParams['bkg->all'] = {'source': 'bkg', 'conds': {'cellType': ['E','I']}, 'weight': 8.0, 'sec': 'soma', 'delay': 'max(1, normal(5,2))', 'synMech': 'exc'}
 
 ## Cell connectivity rules
 netParams.connParams['E->all'] = {
@@ -88,11 +94,13 @@ simConfig.recordStep = 1             # Step size in ms to save data (eg. V trace
 simConfig.filename = 'net_lfp'   # Set file output name
 simConfig.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'},
                           'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
-                          'cao_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cao'},
-                          'ik_soma': {'sec': 'soma', 'loc':0.5, 'var': 'ik'}}  # Dict with traces to record
+                          #'cao_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cao'},
+                          'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
+                          'ip3_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ip3i'},
+                          'ica_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ica'}}  # Dict with traces to record
                           # 'V_Adend3':{'sec': 'Adend3','loc': 0.5,'var': 'v'},
                           # 'cai_Adend3': {'sec': 'Adend3', 'loc':0.5, 'var': 'cai'},
-                          # 'ica_Adend3': {'sec': 'Adend3', 'loc':0.5, 'var': 'ica'}}  # Dict with traces to record
+                          # }  # Dict with traces to record
 
 simConfig.recordLFP = [[-15, y, 1.0*netParams.sizeZ] for y in range(netParams.sizeY/5, netParams.sizeY, netParams.sizeY/5)]
 
@@ -105,7 +113,12 @@ if createSimulate:
     # --------------------------------
     # Instantiate network
     # --------------------------------
-    sim.create()
+    sim.initialize(netParams, simConfig)  # create network object and set cfg and net params
+    sim.net.createPops()                  # instantiate network populations
+    sim.net.createCells()                 # instantiate network cells based on defined populations
+    sim.net.connectCells()                # create connections between cells based on params
+    sim.net.addStims()                    # add external stimulation to cells (IClamps etc)
+    
 
     # --------------------------------
     # Add RxD
@@ -114,11 +127,11 @@ if createSimulate:
     if addRxD:
         caDiff = 0.08
         ip3Diff = 1.41
-        cac_init = 1e-5 # 1.e-4
-        ip3_init = 0.0
-        gip3r = 12040 * 1e3
+        cac_init = 1e-4 #1e-2# 1e-5 # 1.e-4  # ~100 nM (100k less than extracellular); 1000 nM when increased
+        ip3_init = 0 #1e-6
+        gip3r = 12040 * 100
         gserca = 0.3913
-        gleak = 6.020 * 0.01 #1e6 #100
+        gleak = 6.020 #* 0.01 #1e6 #100
         kserca = 0.1
         kip3 = 0.15
         kact = 0.4
@@ -148,6 +161,9 @@ if createSimulate:
         # h.finitialize(-65)  
         # h.dt *= 10
 
+        # t_vec, ip3_vec = h.Vector(), h.Vector()
+        # t_vec.record(h._ref_t)
+        # ip3_vec.record(sim.net.cells[0].secs['soma']['hSec'](0.5)._ref_ip3i)
 
 
         def init_rxd(ca, ip3, cac_init, fc, fe):
@@ -162,6 +178,78 @@ if createSimulate:
             #     print node.concentration
 
         sim.fih.append(h.FInitializeHandler((init_rxd, (ca, ip3, cac_init, fc, fe))))
+
+
+    # --------------------------------
+    # Add extracellular
+    # --------------------------------
+    addExtra = True
+    if addExtra:
+        cyt_extra = rxd.Region(h.allsec(), nrn_region='i')
+        rxd.options.enable.extracellular = True
+        extracellular = rxd.Extracellular(xlo=-10, ylo=-10, zlo=-10, xhi = netParams.sizeX, yhi = netParams.sizeZ, zhi = netParams.sizeY, dx=5, volume_fraction=0.2, tortuosity=1.6) #vol_fraction and tortuosity associated w region 
+        na = rxd.Species([extracellular,cyt_extra], name= 'na', charge= 1, d=1.78)
+        k = rxd.Species([extracellular, cyt_extra], name = 'k', charge = 1, d =1.78)
+
+        # h.finitialize()
+        # print 'initial state %g' % rxd_na[extracellular].states3d.mean()
+        # na = rxd_na[cyt_extra]
+        # k = rxd_k[cyt_extra]
+        # na_ecs = rxd_na[extracellular]
+        # k_ecs = rxd_k[extracellular]
+        # na_ecs.states3d[:] = 140
+        # k_ecs.states3d[:] = 4
+        #rxd_na[extracellular].states3d[:] = h.nao0_na_ion
+        #rxd_k[extracellular].states3d[:] = h.ko0_k_ion
+
+
+    # --------------------------------
+    # Simulate and analyze network
+    # --------------------------------
+    sim.setupRecording()             # setup variables to record for each cell (spikes, V traces, etc)
+    sim.simulate()
+    sim.analyze()
+
+
+    # --------------------------------
+    # Plot RxD
+    # --------------------------------
+    from matplotlib import pyplot
+    from matplotlib_scalebar import scalebar
+
+    pyplot.figure()
+    pyplot.imshow(k[extracellular].states3d[:].mean(2), interpolation='nearest', origin='upper')  #  extent=k[extracellular].extent('xy')
+    sb = scalebar.ScaleBar(1e-6)
+    sb.location='lower left'
+    ax = pyplot.gca()
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    pyplot.xlabel('x')
+    pyplot.ylabel('y')
+    ax.add_artist(sb)
+    pyplot.colorbar(label="$K^+$ (mM)")
+
+    pyplot.figure()
+    pyplot.imshow(k[extracellular].states3d[:].mean(1), interpolation='nearest', origin='upper')  #  extent=k[extracellular].extent('xy')
+    sb = scalebar.ScaleBar(1e-6)
+    sb.location='lower left'
+    ax = pyplot.gca()
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    pyplot.xlabel('x')
+    pyplot.ylabel('z')
+    ax.add_artist(sb)
+    pyplot.colorbar(label="$K^+$ (mM)")
+
+    pyplot.show()
+
+
+
+# for node in ip3.nodes:
+#     print node.concentration
+
+# Create network and run simulation
+#sim.createSimulateAnalyze(netParams = netParams, simConfig = simConfig)    
 
 
   # ######################### start RYR ####################################################################################
@@ -194,43 +282,6 @@ if createSimulate:
   #   ryr_gate = w_state[cyt_er_membrane] * (1.0 + c3ryr) / (1.0 + c4ryr + c3ryr)
   #   # the following is extracted from equ 9 and 15
   #   k_ryr = v1ryr*ryr_gate
-  #   ryr = rxd.MultiCompartmentReaction(ca[er]<>ca[cyt], k_ryr, k_ryr, membrane=cyt_er_membrane)
+  #   ryr = rxd.MultiCompartmentReaction(ca[er], ca[cyt], k_ryr, k_ryr, membrane=cyt_er_membrane)
   # ######################### end RYR ####################################################################################
 
-
-
-    # --------------------------------
-    # Add extracellular
-    # --------------------------------
-    addExtra = False
-    if addExtra:
-        cyt = rxd.Region(h.allsec(), nrn_region='i')
-        rxd.options.enable.extracellular = True
-        extracellular = rxd.Extracellular(xlo=-2, ylo=-300, zlo=-2, xhi = 300, yhi =0, zhi =100, dx=5, volume_fraction=0.2, tortuosity=1.6) #vol_fraction and tortuosity associated w region 
-        rxd_na = rxd.Species([extracellular,cyt], name= 'na', charge= 1, d=1.78)
-        rxd_k = rxd.Species([extracellular, cyt], name = 'k', charge = 1, d =1.78)
-        h.finitialize()
-        print 'initial state %g' % rxd_na[extracellular].states3d.mean()
-        na = rxd_na[cyt]
-        k = rxd_k[cyt]
-        na_ecs = rxd_na[extracellular]
-        k_ecs = rxd_k[extracellular]
-        na_ecs.states3d[:] = 140
-        k_ecs.states3d[:] = 4
-        rxd_na[extracellular].states3d[:] = h.nao0_na_ion
-        rxd_k[extracellular].states3d[:] = h.ko0_k_ion
-
-
-    # --------------------------------
-    # Simulate and analyze network
-    # --------------------------------
-
-    sim.simulate()
-    sim.analyze()
-
-
-for node in ip3.nodes:
-    print node.concentration
-
-# Create network and run simulation
-#sim.createSimulateAnalyze(netParams = netParams, simConfig = simConfig)    
