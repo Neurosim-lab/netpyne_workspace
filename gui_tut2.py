1a1b8e8ce7fdf9ba175f93f5c33f49b8d84fdd3f
salvadord <salvadordura@gmail.com>
2018-07-11 14:29:16 -0400

    removed recordCells=[0] from tut2

--- gui_tut2.py
+++ gui_tut2.py
@@ -49,7 +49,6 @@ simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict w
 simConfig.recordStep = 1 			# Step size in ms to save data (eg. V traces, LFP, etc)
 simConfig.filename = 'model_output'  # Set file output name
 simConfig.savePickle = False 		# Save params, network and sim output to pickle file
-simConfig.recordCells = [0]
 
 #simConfig.analysis['plotSpikeHist'] =  {'include': ['E','I'], 'yaxis': 'count'}
 simConfig.analysis['plotTraces']={'include': [0,4], 'oneFigPer': 'trace'}
