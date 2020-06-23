92338da3f056bbf13cea61027cff5d911479a17c
salvadord <salvadordura@gmail.com>
2020-06-22 21:09:46 -0400

    added new cell for tut2

--- gui_tut2.py
+++ gui_tut2.py
@@ -5,12 +5,12 @@ from netpyne import specs
 netParams = specs.NetParams()  # object of class NetParams to store the network parameters
 
 ## Cell property rules
-netParams.importCellParams(label='PT', fileName='cells/PTcell.hoc', cellName='PTcell', conds={'cellType': 'PT'}, somaAtOrigin=True) 
-netParams.importCellParams(label='FS', fileName='cells/FScell.hoc', cellName='FScell', conds={'cellType': 'FS'}) 
+netParams.importCellParams(label='PT', fileName='cells/PTcell.hoc', cellName='PTcell') 
+netParams.importCellParams(label='SRI', fileName='cells/SRI.hoc', cellName='SRI') 
 
 ## Population parameters
 netParams.popParams['E'] = {'cellType': 'PT', 'numCells': 3}
-netParams.popParams['I'] = {'cellType': 'FS', 'numCells': 10}
+netParams.popParams['I'] = {'cellType': 'SRI', 'numCells': 3}
 
 
 # Stimulation parameters
@@ -19,20 +19,19 @@ netParams.stimTargetParams['bkg->PYR1'] = { 'source': 'bkg',
                                             'conds': {'pop': ['E']},
                                             'sec': 'apic_1',
                                             'synMech': 'AMPA',
-                                            'weight': 0.08,
+                                            'weight': 0.2,
                                             'delay': 5}
 
-
 # Synaptic mechanism parameters
-netParams.synMechParams['AMPA'] = {'mod': 'Exp2Syn', 'tau1': 0.05, 'tau2': 5.3, 'e': 0}
-netParams.synMechParams['GABA'] = {'mod': 'Exp2Syn', 'tau1': 0.5, 'tau2': 1.0, 'e': -90}
+netParams.synMechParams['AMPA'] = {'mod': 'Exp2Syn', 'tau1': 0.01, 'tau2': 0.5, 'e': 20}
+netParams.synMechParams['GABA'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 18, 'e': -90}
 
 
 # Connectivity parameters
 netParams.connParams['E->I'] = {
     'preConds': {'pop': 'E'},
-    'postConds': {'pop': ['I']},
-    'weight': 0.03,                    # weight of each connection
+    'postConds': {'pop': 'I'},
+    'weight': 0.01,  # weight of each connection
     'delay': 5,     
     'synMech': 'AMPA',
     'sec': 'soma'}
@@ -40,8 +39,8 @@ netParams.connParams['E->I'] = {
 netParams.connParams['I->E'] = {
      'preConds': {'pop': 'I'},
      'postConds': {'pop': ['E']},
-     'weight': 0.4,                    # weight of each connection
-     'delay': 10,     
+     'weight': 0.01,                    # weight of each connection
+     'delay': 5,     
      'synMech': 'GABA',
      'sec': 'soma'}
 
@@ -51,13 +50,14 @@ simConfig = specs.SimConfig()		# object of class SimConfig to store simulation c
 
 simConfig.duration = 0.5*1e3 			# Duration of the simulation, in ms
 simConfig.dt = 0.1			# Internal integration timestep to use
-simConfig.verbose = False  			# Show detailed messages 
+simConfig.hParams['celsius'] = 34
+simConfig.verbose = False  # Show detailed messages 
 simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
-simConfig.recordStep = 1 			# Step size in ms to save data (eg. V traces, LFP, etc)
+simConfig.recordStep = 0.1 			# Step size in ms to save data (eg. V traces, LFP, etc)
 simConfig.filename = 'model_output'  # Set file output name
 simConfig.savePickle = False 		# Save params, network and sim output to pickle file
 
-simConfig.analysis['iplotSpikeHist'] =  {'include': ['E','I'], 'yaxis': 'count', 'showFig': True}
+#simConfig.analysis['iplotRaster'] =  {'showFig': True}
 simConfig.analysis['iplotTraces'] = {'include': [0,4], 'oneFigPer': 'trace'}
 
 from netpyne import sim
